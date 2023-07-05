e750f082a0f8f49a143bf68200e8d2680f53b905
omkaryadv177 <97588463+omkaryadv177@users.noreply.github.com>
2023-07-05 13:32:58 +0530

    Update install_java_18.sh

--- ci/install_java_18.sh
+++ ci/install_java_18.sh
@@ -8,7 +8,8 @@ java_extract_dir="/tmp/java-18"
 curl -L "$java_download_url" -o "$java_file_name"
 
 # Extract the Java installation file
-tar -xzf "$java_file_name" -C "$java_extract_dir"
+mkdir -p "$java_extract_dir"
+pax -r -f "$java_file_name" -s ',^\.,'"$java_extract_dir"','
 
 # Set the JAVA_HOME environment variable
 export JAVA_HOME="$java_extract_dir/jdk-18"
