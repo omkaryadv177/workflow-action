98f14f5023ae6e396316cbdc0a19cc15d2951ede
omkaryadv177 <97588463+omkaryadv177@users.noreply.github.com>
2023-07-05 13:21:40 +0530

    Update install_java_18.py

--- ci/install_java_18.py
+++ ci/install_java_18.py
@@ -1,23 +1,23 @@
 import os
 
 def install_java_18():
-  """Installs Java 18 on macOS."""
+    """Installs Java 18 on macOS."""
 
-  java_download_url = "https://jdk.java.net/18/"
-  java_file_name = "jdk-18.tar.gz"
-  java_extract_dir = "/tmp/java-18"
+    java_download_url = "https://jdk.java.net/18/"
+    java_file_name = "jdk-18.tar.gz"
+    java_extract_dir = "/tmp/java-18"
 
-  # Download the Java installation file.
-  os.system(f"curl -L '{java_download_url}' -o '{java_file_name}'")
+    # Download the Java installation file.
+    os.system("curl -L {} -o {}".format(java_download_url, java_file_name))
 
-  # Extract the Java installation file.
-  os.system(f"tar -xzf '{java_file_name}' -C '{java_extract_dir}'")
+    # Extract the Java installation file.
+    os.system("tar -xzf {} -C {}".format(java_file_name, java_extract_dir))
 
-  # Set the `JAVA_HOME` environment variable.
-  os.environ["JAVA_HOME"] = f"'{java_extract_dir}'/jdk-18"
+    # Set the `JAVA_HOME` environment variable.
+    os.environ["JAVA_HOME"] = "{}/jdk-18".format(java_extract_dir)
 
-  # Verify that Java 18 is installed.
-  os.system("java -version")
+    # Verify that Java 18 is installed.
+    os.system("java -version")
 
 if __name__ == "__main__":
-  install_java_18()
+    install_java_18()
