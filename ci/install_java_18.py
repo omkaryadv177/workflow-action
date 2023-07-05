1bbe0d135e4da0c7b11dcf5fd9a9f17859ed85ab
omkaryadv177 <97588463+omkaryadv177@users.noreply.github.com>
2023-07-05 13:09:30 +0530

    Update install_java_18.py

--- ci/install_java_18.py
+++ ci/install_java_18.py
@@ -8,13 +8,13 @@ def install_java_18():
   java_extract_dir = "/tmp/java-18"
 
   # Download the Java installation file.
-  os.system(f"curl -L {java_download_url} -o {java_file_name}")
+  os.system(f"curl -L '{java_download_url}' -o '{java_file_name}'")
 
   # Extract the Java installation file.
-  os.system(f"tar -xzf {java_file_name} -C {java_extract_dir}")
+  os.system(f"tar -xzf '{java_file_name}' -C '{java_extract_dir}'")
 
   # Set the `JAVA_HOME` environment variable.
-  os.environ["JAVA_HOME"] = f"{java_extract_dir}/jdk-18"
+  os.environ["JAVA_HOME"] = f"'{java_extract_dir}'/jdk-18"
 
   # Verify that Java 18 is installed.
   os.system("java -version")
